df413d2c87f02e780607ab3a0d81328408fb8427
dvalinn <tiago.andre.amorim@gmail.com>
2024-07-04 17:33:52 +0100

    modular approach success

--- flake.nix
+++ flake.nix
@@ -1,35 +1,39 @@
 {
-  description = "Nixos config flake";
+	description = "Nixos config flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+		home-manager = {
+			url = "github:nix-community/home-manager";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
 
-  outputs = { self, nixpkgs, home-manager, ... } @ inputs: {
-    nixosConfigurations = {
+	outputs = { self, nixpkgs, home-manager, ... } @ inputs: {
+		nixosConfigurations = {
 
-    	default = nixpkgs.lib.nixosSystem {
+			default = nixpkgs.lib.nixosSystem {
 
-			specialArgs = {inherit inputs;};
-			modules = [
-				./hosts/default
+				specialArgs = {inherit inputs;};
+				modules = [
+					# Not specifying the file, makes nix search for a default.nix
+					# in this case ./hosts/default/default.nix must be present
+					./hosts/default
 
-				home-manager.nixosModules.home-manager{
-					home-manager.useGlobalPkgs = true;
-					home-manager.useUserPackages = true;
+					home-manager.nixosModules.home-manager {
+						home-manager.useGlobalPkgs = true;
+						home-manager.useUserPackages = true;
 
-					home-manager.extraSpecialArgs = inputs;
-					home.manager.users.dvalinn = import ./home;
+						home-manager.extraSpecialArgs = inputs;
+						# same as above. ./home/default.nix must be present
+						home-manager.users.dvalinn = import ./home/default.nix;
 
-				}
-			];
+					}
+				];
+
+			};
 
 		};
-    	
 	};
 }

05fdf2549d4045c82a06c2f8371e87c823478b19
dvalinn <tiago.andre.amorim@gmail.com>
2024-07-09 14:35:09 +0100

    workig on xserver configuration

--- hosts/nix-desktop/default.nix
+++ hosts/nix-desktop/default.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ lib, ... }:
+{ ... }:
 
 {
   imports =
@@ -17,11 +17,6 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  # override xserver settings with custom xorg.conf
-  services.xserver.displayManager.xserverArgs = lib.mkAfter [
-    "-config" "${builtins.readFile ./xorg.conf}"
-  ];
 
   networking.hostName = "nix-desktop"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -40,6 +35,40 @@
 
   services.xserver.videoDrivers = ["nvidia"];
   hardware.nvidia.modesetting.enable = true;
+
+  # xserver settings
+  services.xserver = 
+  {
+    xrandrHeads = [
+      {
+        output = "DP-0";
+        primary = true;
+        monitorConfig = ''
+          Identifier     "Monitor0"
+        	ModelName      "Acer KG271 A"
+          Horizsync       180.0 - 180.0
+          VertRefresh     30.0 - 144.0
+          Option         "DPMS" "true"
+        '';
+      }
+      {
+        output = "DP-4";
+        monitorConfig = ''
+          Indentifier    "Monitor1"
+          ModelName      "Asustek Computer Inc VG248"
+          HorizSync       193.0 - 193.0
+          VertRefresh     48.0 - 165.0
+          Option         "DPMS"
+        '';
+      }
+    ];
+
+    screenSection = ''
+      DefaultDepth   24
+      Option         "metamodes"  "DP-4: 1920x1080_144 +1920+0, DP-0: 1920x1080_144 +0+0 {AllowGSYNCCompatible=On}"
+      '';
+  };
+
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions

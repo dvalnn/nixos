dc57fcccac6079901874a7f2ae5bf1c59de0f0ce
dvalinn <tiago.andre.amorim@gmail.com>
2024-07-21 16:59:36 +0100

    hyprland keys

--- homeManagerModules/windowManagers/hyprland/keys.nix
+++ homeManagerModules/windowManagers/hyprland/keys.nix
@@ -7,21 +7,26 @@
     bind = [
       "$mainMod, Return, exec, $terminal"
       "$mainMod, E, exec, $fileManager"
-      "$mainMod, R, exec, $menu"
+      "$mainMod, Space, exec, $menu"
       "$mainMod, B, exec, $browser"
 
       "$mainMod, Q, killactive,"
-      "$mainMod_Shift, Q, exit,"
+      "$mainMod SHIFT, Q, exit,"
 
       "$mainMod, P, pseudo," # dwindle
       "$mainMod, O, togglesplit," # dwindle
       "$mainMod, V, togglefloating,"
 
-      # Move focus with mainMod + arrow keys or HJKL
-      "$mainMod, left&H, movefocus, l"
-      "$mainMod, right&L, movefocus, r"
-      "$mainMod, up&K, movefocus, u"
-      "$mainMod, down&J, movefocus, d"
+      # Move focus with mainMod + arrow keys 
+      "$mainMod, left, movefocus, l"
+      "$mainMod, right, movefocus, r"
+      "$mainMod, up, movefocus, u"
+      "$mainMod, down, movefocus, d"
+
+      "$mainMod, H, movefocus, l"
+      "$mainMod, L, movefocus, r"
+      "$mainMod, K, movefocus, u"
+      "$mainMod, J, movefocus, d"
 
       # Switch workspaces with mainMod + [0-9]
       "$mainMod, 1, workspace, 1"
@@ -47,6 +52,22 @@
       "$mainMod SHIFT, 9, movetoworkspace, 9"
       "$mainMod SHIFT, 0, movetoworkspace, 10"
 
+      # "$mainMod SHIFT ALT, 1, movetoworkspacesilent, 1"
+      # "$mainMod SHIFT ALT, 2, movetoworkspacesilent, 2"
+      # "$mainMod SHIFT ALT, 3, movetoworkspacesilent, 3"
+      # "$mainMod SHIFT ALT, 4, movetoworkspacesilent, 4"
+      # "$mainMod SHIFT ALT, 5, movetoworkspacesilent, 5"
+      # "$mainMod SHIFT ALT, 6, movetoworkspacesilent, 6"
+      # "$mainMod SHIFT ALT, 7, movetoworkspacesilent, 7"
+      # "$mainMod SHIFT ALT, 8, movetoworkspacesilent, 8"
+      # "$mainMod SHIFT ALT, 9, movetoworkspacesilent, 9"
+      # "$mainMod SHIFT ALT, 0, movetoworkspacesilent, 10"
+
+      "$mainMod ALT, L, focusmonitor, l"
+      "$mainMod ALT, H, focusmonitor, r"
+      "$mainMod ALT, K, focusmonitor, u"
+      "$mainMod ALT, J, focusmonitor, d"
+
       # Example special workspace (scratchpad)
       "$mainMod, S, togglespecialworkspace, magic"
       "$mainMod SHIFT, S, movetoworkspace, special:magic"
@@ -55,6 +76,14 @@
       "$mainMod, mouse_down, workspace, e+1"
       "$mainMod, mouse_up, workspace, e-1"
     ];
+
+    # Combo keys
+    # binds = [
+    #   "$mainMod, W&L, swapactiveworkspaces, current l"
+    #   "$mainMod, W&H, swapactiveworkspaces, current r"
+    #   "$mainMod, W&K, swapactiveworkspaces, current u"
+    #   "$mainMod, W&K, swapactiveworkspaces, current d"
+    # ];
 
     # Mouse bindings
     bindm = [

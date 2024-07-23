2b8de6c6f03fcc02068210f98c49bc77653f8085
dvalinn <tiago.andre.amorim@gmail.com>
2024-07-23 02:58:03 +0100

    changed default wallpaper. removing swww autostart.
    changed default wallpaper. removing swww autostart.
    
    changed default wallpaper. removing swww autostart.
    
    changed default wallpaper. removing swww autostart.

--- homeManagerModules/windowManagers/hyprland/hyprconf/autostart.nix
+++ homeManagerModules/windowManagers/hyprland/hyprconf/autostart.nix
@@ -3,15 +3,9 @@
     exec-once = [
       "ags"
 
-      # start side monitor on workspace 6
-      "hyprctl dispatch focusmonitor 0"
-      "hyprctl dispatch workspace 6"
-
       # start main monitor on workspace 1
       "hyprctl dispatch focusmonitor 1"
       "hyprctl dispatch workspace 1"
-
-      # "~/.config/hypr/scripts/wallpaperdaemon.sh"
     ];
   };
 }

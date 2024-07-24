1d03f1f864d2b36373b83be7399d16ed2b230af4
dvalinn <tiago.andre.amorim@gmail.com>
2024-07-24 16:45:04 +0100

    changed awesome config to use awesome-copycats

---
+++ homeManagerModules/windowManagers/awesome/config-backup/rc.lua
@@ -0,0 +1,42 @@
+local awful = require("awful")
+local menubar = require("menubar")
+local beautiful = require("beautiful")
+
+require("awful.hotkeys_popup.keys")
+require("awful.autofocus")
+require("startup")
+
+local const = require("settings").constants
+local apps = require("settings").default_apps
+local keys = require("keys")
+local buttons = require("buttons")
+
+local theme_path = string.format(
+    "%s/.config/awesome/themes/%s/theme.lua",
+    const.HOME,
+    const.THEME
+)
+
+beautiful.init(theme_path)
+
+root.keys(keys.global)
+root.buttons(buttons.root)
+
+menubar.utils.terminal      = apps.terminal -- Set the terminal for applications that require it
+
+-- Table of layouts to cover with awful.layout.inc, order matters.
+awful.layout.layouts        = {
+    awful.layout.suit.floating,
+    awful.layout.suit.tile,
+    awful.layout.suit.tile.left,
+    awful.layout.suit.fair,
+    awful.layout.suit.spiral.dwindle,
+    awful.layout.suit.max,
+}
+
+awful.util.tagnames         = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }
+awful.util.taglist_buttons  = buttons.taglist
+awful.util.tasklist_buttons = buttons.tasklist.default
+
+require("client.signals")
+require("screen")
--- homeManagerModules/windowManagers/awesome/default.nix
+++ homeManagerModules/windowManagers/awesome/default.nix
@@ -1,8 +1,22 @@
-{...}: {
+{pkgs, ...}: {
   home.file = {
     ".config/awesome" = {
       source = ./config;
       recursive = true;
     };
   };
+
+  # Rice dependencies
+  home.packages = with pkgs; [
+    terminus_font
+    alsa-utils # amixer
+    dmenu
+    mpc-cli
+    mpd
+    scrot
+    unclutter
+    xorg.xbacklight
+    xsel
+    slock
+  ];
 }

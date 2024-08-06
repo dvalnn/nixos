ddb5d8924bfc48f16eca50a70041b13f1c038733
dvalinn <tiago.andre.amorim@gmail.com>
2024-08-06 13:20:17 +0100

    tmux config and fzf integration

--- homeManagerModules/cliPrograms/fzf.nix
+++ homeManagerModules/cliPrograms/fzf.nix
@@ -11,6 +11,12 @@
     programs.fzf= {
       enable = true;
       enableZshIntegration = if config.zsh.enable then true else false;
+      tmux = {
+        enableShellIntegration = if config.tmux.enable then true else false;
+        shellIntegrationOptions = [ 
+          "-p" # run in popup mode
+        ]; # see fzf-tmux --help for available options
+      };
     };
   };
 }

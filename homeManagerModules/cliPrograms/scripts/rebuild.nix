d7ddcdc0476bb746c42c274528ea287fcb422ee5
dvalinn <tiago.andre.amorim@gmail.com>
2024-08-06 14:20:21 +0100

    158 current  2024-08-06 14:20:20  24.11.20240804.cb9a96f  6.6.44                          *

--- homeManagerModules/cliPrograms/scripts/rebuild.nix
+++ homeManagerModules/cliPrograms/scripts/rebuild.nix
@@ -4,12 +4,11 @@
       ''
         set -e
         pushd ~/nixos/
-        nvim .
+        nvim
         alejandra . &>/dev/null
         git diff -U0 *.nix
         echo "NixOS Rebuilding..."
-        nh rebuild switch --ask &>nixos-switch.log || (
-        cat nixos-switch.log | grep --color error && false)
+        nh os switch --ask
         gen=$(nixos-rebuild list-generations | grep current)
         git commit -am "$gen"
         popd

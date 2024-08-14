53c6953389e65da890f67387c7bd9162e14cee0b
dvalinn <tiago.andre.amorim@gmail.com>
2024-08-14 15:41:18 +0100

    164 current  2024-08-14 15:41:17  24.11.20240804.cb9a96f  6.6.44                          *

--- homeManagerModules/cliPrograms/scripts/rebuild.nix
+++ homeManagerModules/cliPrograms/scripts/rebuild.nix
@@ -13,5 +13,21 @@
         git commit -am "$gen"
         popd
       '')
+
+    (pkgs.writeShellScriptBin "update"
+      ''
+        function reset_flake {
+          pushd ~/nixos/
+          git reset --hard
+          popd
+        }
+
+        trap reset_flake ERR
+
+        echo "NixOS Updating..."
+        nh os switch --update --ask
+        gen=$(nixos-rebuild list-generations | grep current)
+        git commit -am "$gen"
+      '')
   ];
 }

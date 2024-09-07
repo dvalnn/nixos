4c83d5b5a571de441a92056305995cac024da07c
dvalinn <tiago.andre.amorim@gmail.com>
2024-09-07 21:35:01 +0100

    182 current  2024-09-07 21:35:00  24.11.20240831.12228ff  6.6.48                          *

--- homeManagerModules/cliPrograms/zellij/default.nix
+++ homeManagerModules/cliPrograms/zellij/default.nix
@@ -16,33 +16,10 @@
         else false;
       settings = {
         layout = "compact";
+        plugins = {
+          forgot.location = "https://github.com/karimould/zellij-forgot/releases/download/0.4.0/zellij_forgot.wasm";
+        };
       };
     };
-
-    # stylix.targets.zellij.enable = false;
-    # home.file = let
-    #   colors = config.lib.stylix.colors.withHashtag;
-    # in {
-    #   ".config/zellij/config.kdl".text = ''
-    #     ${builtins.readFile ./config.kdl}
-    #
-    #     theme "stylix"
-    #     themes {
-    #       stylix {
-    #         bg ${colors.base00}
-    #         black ${colors.black}
-    #         blue ${colors.blue}
-    #         cyan ${colors.cyan}
-    #         fg ${colors.foreground}
-    #         green ${colors.green}
-    #         magenta ${colors.magenta}
-    #         orange ${colors.orange}
-    #         red ${colors.base}
-    #         white ${colors.white}
-    #         yellow ${colors.yellow}
-    #       }
-    #     }
-    #   '';
-    # };
   };
 }

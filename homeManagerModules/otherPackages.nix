a1744c37c40f6b189de2fda9e9e9321b04ea0c84
dvalinn <tiago.andre.amorim@gmail.com>
2024-10-05 15:33:19 +0100

    215 current  2024-10-05 15:33:18  24.11.20241004.bc947f5  6.6.53                          *

--- homeManagerModules/otherPackages.nix
+++ homeManagerModules/otherPackages.nix
@@ -1,46 +1,39 @@
-{
-  pkgs,
-  pkgs-stable,
-  ...
-}: {
-  home.packages =
-    (with pkgs-stable; [
-      spotify
-      google-chrome
-    ])
-    ++ (with pkgs; [
-      #network management
-      networkmanagerapplet
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    #network management
+    networkmanagerapplet
 
-      # screenshot
-      flameshot
+    # screenshot
+    flameshot
 
-      # other Cli tools
-      alejandra # nix formatter
-      fastfetch
-      ripgrep
-      repgrep
-      lazygit
-      xclip
-      unzip
-      btop
-      less
-      dust
-      bat
-      eza
-      jq
-      fd
-      gh # github cli
+    # other Cli tools
+    alejandra # nix formatter
+    fastfetch
+    ripgrep
+    repgrep
+    lazygit
+    xclip
+    unzip
+    btop
+    less
+    dust
+    bat
+    eza
+    jq
+    fd
+    gh # github cli
 
-      # GUI apps
-      firefox
-      vesktop # alternative discord client
-      stremio
-      obsidian
-      vscode-fhs
+    # GUI apps
+    firefox
+    vesktop # alternative discord client
+    stremio
+    obsidian
+    vscode-fhs
+    spotify
+    google-chrome
 
-      # File management
-      yazi
-      xfce.thunar
-    ]);
+    # File management
+    yazi
+    xfce.thunar
+  ];
 }

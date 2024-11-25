a9264a9ccc4cd2f85d16f8a14be0e66fe58d986a
dvalinn <tiago.andre.amorim@gmail.com>
2024-11-25 15:42:16 +0000

    55 current  2024-11-25 15:42:15  25.05.20241115.5e4fbfb  6.11.8                          *

--- homeManagerModules/cliPrograms/nvim/config/lua/after/lang/init.lua
+++ homeManagerModules/cliPrograms/nvim/config/lua/after/lang/init.lua
@@ -1,6 +1,7 @@
 require("after.lang.lsp")
 require("after.lang.cmp")
 require("after.lang.rust")
+require("after.lang.java")
 
 require("nvim-treesitter.configs").setup{
     -- Do not ensure installed here, grammar packs are installed through nix

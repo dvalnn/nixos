7bb5dcb1d7098f2c8758a4178b559f8f2eb13155
dvalinn <tiago.andre.amorim@gmail.com>
2025-08-02 12:41:37 +0100

    384 current 2025-08-02 25.11.20250731.94def63 6.16.0

--- flake.nix
+++ flake.nix
@@ -4,9 +4,15 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
+
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.3-1.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
     };
 
     home-manager = {
@@ -63,7 +69,8 @@
           modules = [
             lix-module.nixosModules.default
             ./hosts/nix-laptop
-          ] ++ homeConfig ./homeManagerModules/laptop.nix;
+          ]
+          ++ homeConfig ./homeManagerModules/laptop.nix;
         };
 
         nix-desktop = nixpkgs.lib.nixosSystem {
@@ -74,7 +81,8 @@
           modules = [
             lix-module.nixosModules.default
             ./hosts/nix-desktop
-          ] ++ homeConfig ./homeManagerModules/desktop.nix;
+          ]
+          ++ homeConfig ./homeManagerModules/desktop.nix;
         };
       };
     };

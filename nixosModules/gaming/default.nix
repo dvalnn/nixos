aad36e5c961d1fd5f44a018740e497ef4958b641
dvalinn <tiago.andre.amorim@gmail.com>
2025-11-09 14:44:08 +0000

    404 current 2025-11-09 25.11.20251108.b6a8526 6.17.7

--- nixosModules/gaming/default.nix
+++ nixosModules/gaming/default.nix
@@ -21,9 +21,9 @@
     hamachi.enable = lib.mkDefault false;
     sunshine.enable = lib.mkDefault false;
 
-    # environment.systemPackages = with pkgs; [
-    #   # mangohud # msi afterburner monitor thingy
-    #   # bottles # wine presets
-    # ];
+    environment.systemPackages = with pkgs; [
+      mangohud # msi afterburner monitor thingy
+      bottles # wine presets
+    ];
   };
 }
